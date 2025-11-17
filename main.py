13991650801941ec5338ccb0dccc3a567f2a86fd
pcmagas <pcmagas@disroot.org>
2025-11-17 20:10:19 +0200

    Add basic io

---
+++ main.py
@@ -0,0 +1,20 @@
+import spidev
+
+# Create SPI object
+spi = spidev.SpiDev()
+spi.open(0, 0)  # bus 0, device 0
+spi.max_speed_hz = 50000
+
+device_id=0xC212
+
+# Send and receive data
+resp = spi.xfer2([0x09])
+
+if(resp == device_id):
+    print("Macronix MX35LF1GE4AB found")
+    print("Device Id:"+resp) 
+else:
+    print("Device not found")
+    exit(1)
+
+spi.close()
