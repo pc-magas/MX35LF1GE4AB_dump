6248a18036dd0801bce239ae91f47118157c4f96
pc_magas <pcmagas@disroot.org>
2025-11-17 21:10:37 +0200

    Commit in order to fix tab

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ spi = spidev.SpiDev()
 spi.open(0, 0)  # bus 0, device 0
 spi.max_speed_hz = 50000
 
-device_id=0xC212
-
 # Send and receive data
-resp = spi.xfer2([0x09])
+resp = spi.xfer2([0x9F, 0x00, 0x00, 0x00])
 print(resp)
-if(resp == device_id):
+if(resp[2] == 0xC2):
     print("Macronix MX35LF1GE4AB found")
-    print("Device Id:"+resp) 
+    if(resp[3] == 0x12):
+	print("Device type is SERIAL NAND")
+    else:
+	print("Uknown type")
+	exit(1)
 else:
     print("Device not found")
     exit(1)
